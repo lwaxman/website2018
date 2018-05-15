80f2998a3e25893f35b9642744394c24f6cc464a
Laurie Waxman <laurie@midnightcommercial.com>
2018-05-15 16:07:53 -0400

    added pages

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function(){
+
+	var menuOpen = false; 
+
+	$("#menuToggle").on('click', function(event){
+		menuOpen =! menuOpen; 
+		if(menuOpen){
+			$('.buttons').css("left", "0px");
+		}else{
+			$('.buttons').css("left", "100vw");
+		}
+	});
+
+
+	$('#workButton').on('click', function(event){
+		if( $(this).hasClass("index") ){
+		    event.preventDefault();
+		}
+		
+	    $('html, body').animate({
+	        scrollTop: $('#work').offset().top
+	    }, 500);
+	    if(menuOpen && window.innerWidth < 992){
+	    	$('.buttons').css("left", "100vw");
+	    	menuOpen =! menuOpen; 
+	    }
+	});
+
+	$(window).resize(function() {
+		console.log(window.innerWidth);
+		if( window.innerWidth >= 992){
+			$('.buttons').css("left", "auto");
+		}else{
+			$('.buttons').css("left", "100vw");
+		}
+	});
+
+});
